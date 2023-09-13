977cdf697a4ac1ce200546637dcc2a1bec72d523
Carlos Aliaga <carlosaligom@gmai.com>
2023-09-13 12:46:53 +0200

    Add theme interface and theme to styles

---
+++ src/styles/theme/theme.ts
@@ -0,0 +1,18 @@
+import { DefaultTheme } from "styled-components";
+
+const theme: DefaultTheme = {
+  color: {
+    lightBlue: "#ECF9FD",
+    darkBlue: "#246B83",
+    midBlue: "#C4ECFA",
+    areaBlue: "#42C3EE",
+    darkText: "#14143F",
+    lightText: "#595977",
+    lightRed: "#D60000",
+  },
+  font: {
+    primary: "'Inter', sans-serif",
+  },
+};
+
+export default theme;
