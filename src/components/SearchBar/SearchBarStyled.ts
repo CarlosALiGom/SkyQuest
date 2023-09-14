475064e4ea8530c4304174ee8962caea4e5e32c4
Carlos Aliaga <carlosaligom@gmai.com>
2023-09-14 20:05:05 +0200

    Create SearchBar component with styles and add store provider to main

---
+++ src/components/SearchBar/SearchBarStyled.ts
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+const SearchBarStyled = styled.form`
+  display: flex;
+  flex-direction: column;
+  border-radius: 10px;
+  margin: 0 auto;
+  padding: 15px;
+  gap: 15px;
+
+  .form {
+    &__label {
+      color: ${(prop) => prop.theme.color.lightText};
+    }
+
+    &__control {
+      display: flex;
+      gap: 1rem;
+    }
+
+    &__input {
+      background-color: ${(prop) => prop.theme.color.lightBlue};
+      color: ${(prop) => prop.theme.color.darkBlue};
+      border-radius: 0.5rem;
+      outline: 0;
+      height: 2.5rem;
+      width: 200px;
+      padding: 0.5rem;
+      padding-left: 0.75rem;
+      font-size: 1rem;
+
+      &::placeholder {
+        color: ${(prop) => prop.theme.color.darkBlue};
+      }
+    }
+
+    &__button {
+      height: 2.5rem;
+      width: 2.5rem;
+      background-color: transparent;
+      padding-top: 4px;
+      border-radius: 0.5rem;
+      font-size: 1rem;
+    }
+  }
+`;
+
+export default SearchBarStyled;
