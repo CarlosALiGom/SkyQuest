55914580d5160d9d36f87fbcb8a2d39f61d4fee6
Carlos Aliaga <carlosaligom@gmai.com>
2023-09-15 15:01:11 +0200

    Create EmptyMap component

---
+++ src/components/EmptyMap/EmptyMap.tsx
@@ -0,0 +1,11 @@
+import EmptyMapStyled from "./EmptyMapStyled";
+
+const EmptyMap = () => {
+  return (
+    <EmptyMapStyled className="emptyMap">
+      <p className="emptyMap__paragraph">Search a City</p>
+    </EmptyMapStyled>
+  );
+};
+
+export default EmptyMap;
