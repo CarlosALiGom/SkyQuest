c5f63bfb765a270f1c07ef64a5ce84684cc43d56
Carlos Aliaga <carlosaligom@gmai.com>
2023-09-15 18:36:28 +0200

    Implenent NavBar on Layout with conditional render

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,14 +2,19 @@ import { Outlet } from "react-router-dom";
 import ContainerStyled from "../shared/ContainerStyled";
 import Header from "../Header/Header";
 import SearchBar from "../SearchBar/SearchBar";
+import { useAppSelector } from "../../store";
+import NavBar from "../NavBar/NavBar";
 
 const Layout = (): React.ReactElement => {
+  const cords = useAppSelector((state) => state.city.cords);
+
   return (
     <>
       <ContainerStyled>
         <Header />
         <SearchBar />
         <Outlet />
+        {cords.lat !== "" && <NavBar />}
       </ContainerStyled>
     </>
   );
